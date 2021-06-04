4018cc0b38be2cf58624191e69a41cd3cea109c2
UtsaBasu <76468540+UtsaBasu@users.noreply.github.com>
2021-06-04 22:36:36 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "CODEORG";
+const accessCode2 = "PYTHON";
+const accessCode3 = "FALSE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("O E D R O C G", 100,50)
+    fill("lightblue")
+    fill("white")
+    text("Hint: What was my favourite code editor ?", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+text("TRUE / FALSE ?", 100,300)
+text("Hint: State True or False : I liked the classes with Physics Engine", 100,330)
+fill("white")
+text(" H O P Y N T ", 600,50)
+text("Hint: Which software do I know aside from javascript ? ", 600,70)
+}
